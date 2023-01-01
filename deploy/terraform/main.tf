89bb404400d079b2a5ef043f89ce4e6bcb41e5c7
Jason Jaramillo <jason.araque@hotmail.com>
2023-01-01 06:39:50 +0000

    add token to step

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -12,6 +12,9 @@ terraform {
         source  = "hashicorp/aws"
         version = "~> 4.47.0"
       }
+      tfe = {
+        version = "~> 0.38.0"
+      }
     }
 
     required_version = ">= 1.1.0"
@@ -19,4 +22,8 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
+}
+
+provider "tfe" {
+  token = "6PhH2apXdhez8g.atlasv1.sirPDUbJlDZk3XTnel8REmUxSf7MxJ5NL4zG64fYznm6RBa3huXSD7FWpSVtgJ8jOl0"
 }
