e19fac4aa7ae69816fc9352afebfb756de8842be
Jason Jaramillo <jason.araque@hotmail.com>
2023-01-01 16:08:51 +0000

    add env variables

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,16 +1,13 @@
 variable "aws_region" {
   type = string
-  default = "eu-west-1"
 }
 
 variable "aws_account" {
   type = string
-  default = "972670838762"
 }
 
 variable "stage" {
   type = string
-  default = "stg"
 }
 
 variable "db_connection_string" {
