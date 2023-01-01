07d2d2dc2076c8b72c54cd665af871fbea0db4b4
Jason Jaramillo <jason.araque@hotmail.com>
2023-01-01 08:27:03 +0000

    add secrets

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -15,10 +15,8 @@ variable "stage" {
 
 variable "db_connection_string" {
   type = string
-  default = "mongodb+srv://nodeuser:Test1234@cluster0-hmpes.mongodb.net/catering?w=majority"
 }
 
 variable "jwt_secret" {
   type = string
-  default = "aW!t$nKK!*$agbodu9iXzFKR9YsktN6Ea%mz8Uiddopap@H%Ep6C3afF!eyKVKP*SQ@g72qteHJg7xLvFjVG&NqKBRi%r%Zud^o@uj$$@&7MCRKyYseu&BpR*!n%^4Xk"
 }
